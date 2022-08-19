281345e2fca8ece451761bddedcaabed392ac1a5
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-08-19 16:12:41 -0500

    adding task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Using Puppet to create a file
 file { '/tmp/school': #Path file
-  path    => '/tmp/school'
-  mode    => '0744',
+  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
-  }
+  mode    => '0744',
+  path    => '/tmp/school'
+}
