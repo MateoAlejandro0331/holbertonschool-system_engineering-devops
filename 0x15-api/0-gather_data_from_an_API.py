714475456e57dfc10f9f48b9018bf9cd1ddefafb
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-08 18:00:47 -0500

    adding task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,21 +7,23 @@ if __name__ == "__main__":
     todos = "https://jsonplaceholder.typicode.com/todos"
     user = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
     with request.urlopen(todos) as response:
-        with request.urlopen(user) as User:
-            content_user = User.read()
-            content_user = content_user.decode('utf8').replace("'", '"')
-            data_user = json.loads(content_user)
-        task_completed = []
         total_task = 0
         content = response.read()
         content = content.decode('utf8').replace("'", '"')
         data = json.loads(content)
-        for task in data:
-            if task.get('userId') == int(argv[1]):
-                total_task += 1
-                if task.get('completed') is True:
-                    task_completed.append(task)
-        print("Employee {} is done with tasks({}/{}):"
-              .format(data_user['name'], len(task_completed), total_task))
-        for instance in task_completed:
-            print("\t {}".format(instance.get('title')))
+    with request.urlopen(user) as User:
+        content_user = User.read()
+        content_user = content_user.decode('utf8').replace("'", '"')
+        data_user = json.loads(content_user)
+
+    task_completed = []
+    for task in data:
+        if task.get('userId') == int(argv[1]):
+            total_task += 1
+            if task.get('completed') is True:
+                task_completed.append(task)
+    line = "Employee {} is done with tasks({}/{}):"\
+        .format(data_user['name'], len(task_completed), total_task)
+    print(line)
+    for instance in task_completed:
+        print("\t {}".format(instance.get('title')))
